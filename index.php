d85273a87aa46295510035ef599a42e000d9cfc8
Alhakeem <alhakeem.prof@gmail.com>
2018-04-26 16:02:00 +0300

    add viewport meta tag

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <link href="https://fonts.googleapis.com/css?family=Cairo:600" rel="stylesheet">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <style>
      body{
