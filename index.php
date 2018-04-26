4c5548d5d0405309fe408d742c11ca49e8885081
Alhakeem <alhakeem.prof@gmail.com>
2018-04-26 15:22:52 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Cairo:600" rel="stylesheet">
+
+    
+    <style>
+     body{
+       text-align: center;
+       direction: rtl;
+       font-family: 'Cairo', sans-serif;
+     }
+     img{
+       margin-top: 10%;
+     }
+  h2{
+    color: #606060;
+    margin-top: 20px;
+    font-size: 22px;
+  }
+    </style>
+
+  </head>
+
+  <body>
+    
+
+     <img src="logo.png" alt="coretabs">
+     <h2>قريباً... محتوى جديد</h2>
+
+  </body>
+
+</html>
